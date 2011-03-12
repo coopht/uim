2623f10e5adaf9b6bfe6f1d39d40fa50fbaafbf6
alexander.basov <alexander.basov@3a6b9fcd-8827-4f8d-bdb4-b4f6d909dfd1>
2011-03-12 13:55:24 +0000

    Added functionality to manage protocol's id.

--- source/proto/uim-protocols-common.ads
+++ source/proto/uim-protocols-common.ads
@@ -48,21 +48,40 @@ with UIM.Protocols.Statuses;
 
 package UIM.Protocols.Common is
 
+   --  Each protocol, which implements current interface MUST be registered
+   --  in the special protocols registry.
+
    type Common_Protocol is limited interface;
 
-   type Common_Protocol_Access is access all Common_Protocol'Class;
+   type Common_Protocol_Access is access all Common_Protocol;
 
-   procedure Set_Status
+   not overriding procedure Set_Status
     (Self   : not null access Common_Protocol;
      Status : not null access UIM.Protocols.Statuses.Status) is abstract;
 
-   procedure Change_Status
+   not overriding procedure Change_Status
     (Self   : not null access Common_Protocol;
      Status : League.Strings.Universal_String) is abstract;
    --  XXX: Rewrite with appropriate types
 
-   function Get_Protocol_Handler (Self : not null access Common_Protocol)
+   not overriding function Get_Protocol_Handler
+    (Self : not null access Common_Protocol)
       return not null UIM.Protocols.Handlers.Protocol_Handler_Access
         is abstract;
+   --  returns events handler, associated with protocol
+
+   not overriding function Get_Name (Self : not null access Common_Protocol)
+      return League.Strings.Universal_String is abstract;
+   --  returns protocol name, which can be used in application GUI
+
+   not overriding function Get_Id (Self : not null access Common_Protocol)
+      return Positive is abstract;
+   --  returns protocols id,
+   --  which is given to this protocol during registration
+
+   not overriding procedure Set_Id (Self : not null access Common_Protocol;
+                                    Id   : Positive) is abstract;
+   --  this function is called, when protocols registry adds implementor to
+   --  the list of used protocols, and sets assigned id.
 
 end UIM.Protocols.Common;
