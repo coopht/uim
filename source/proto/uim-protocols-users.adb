9f691a999fc2f7a0558c6f7f960e511e9269c031
alexander.basov <alexander.basov@3a6b9fcd-8827-4f8d-bdb4-b4f6d909dfd1>
2011-03-13 09:39:44 +0000

    Added initial implementation of user class

---
+++ source/proto/uim-protocols-users.adb
@@ -0,0 +1,68 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                               UIM Project                                --
+--                                                                          --
+--                Multi protocol instant messenger for unix                 --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2011, Alexander Basov <coopht@gmail.com>                     --
+-- All rights reserved.                                                     --
+--                                                                          --
+-- Redistribution and use in source and binary forms, with or without       --
+-- modification, are permitted provided that the following conditions       --
+-- are met:                                                                 --
+--                                                                          --
+--  * Redistributions of source code must retain the above copyright        --
+--    notice, this list of conditions and the following disclaimer.         --
+--                                                                          --
+--  * Redistributions in binary form must reproduce the above copyright     --
+--    notice, this list of conditions and the following disclaimer in the   --
+--    documentation and/or other materials provided with the distribution.  --
+--                                                                          --
+--  * Neither the name of the Alexander Basov, IE nor the names of its      --
+--    contributors may be used to endorse or promote products derived from  --
+--    this software without specific prior written permission.              --
+--                                                                          --
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      --
+-- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        --
+-- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    --
+-- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     --
+-- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   --
+-- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED --
+-- TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR   --
+-- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF   --
+-- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING     --
+-- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS       --
+-- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.             --
+--                                                                          --
+------------------------------------------------------------------------------
+--  $Revision$ $Date$
+------------------------------------------------------------------------------
+
+with UIM.Protocols.Contact_List_Items;
+with UIM.Protocols.Informations;
+
+package body UIM.Protocols.Users is
+
+   -------------------
+   --  Information  --
+   -------------------
+
+   function Information (Self : User)
+      return UIM.Protocols.Informations.Information is
+   begin
+      return Self.Information;
+   end Information;
+
+   -----------------------
+   --  Set_Information  --
+   -----------------------
+
+   procedure Set_Information (Self : in out User;
+                              Info : UIM.Protocols.Informations.Information) is
+   begin
+      Self.Information := Info;
+   end Set_Information;
+
+end UIM.Protocols.Users;
